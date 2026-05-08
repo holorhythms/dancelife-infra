4fced9d22519dcb432b7fe66d4cba867758fa835
Howard Lopez <holodevelop@gmail.com>
2026-05-07 21:32:40 -0700

    Rename resource

--- modules/app-core/main.tf
+++ modules/app-core/main.tf
@@ -284,7 +284,7 @@ resource "azurerm_application_insights" "app_service_insights" {
   workspace_id        = azurerm_log_analytics_workspace.app_service_insights_workspace.id
   sampling_percentage = var.app_service_sampling_percentage
 }
-resource "azurerm_application_insights_standard_web_test" "app_service_ping_test" {
+resource "azurerm_application_insights_standard_web_test" "dancelife_app_service_ping_test" {
   name                    = "app-service-ping-test"
   description             = "Ping test for the main Dancelife app service. Checks the GET login urls API."
   resource_group_name     = azurerm_resource_group.rg.name
