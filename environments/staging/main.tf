57c6ef74d305ac90239a6436fa146f2fe4d60be1
Howard Lopez <holodevelop@gmail.com>
2026-06-24 12:57:57 -0700

    Modify staging setting

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -24,7 +24,7 @@ module "app_core" {
     # PostgreSQL configuration
     postgres_server_sku_name = "GP_Standard_D2s_v3"
     postgres_config_backup_retention_days = 14
-    postgres_config_geo_redundant_backup_enabled = true
+    postgres_config_geo_redundant_backup_enabled = false
     postgres_config_secure_transport = "ON"
 
     # Storage account configuration
